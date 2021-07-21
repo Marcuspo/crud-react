8bb1383b69384cf98f1a80c242bc3b1dcd36f1c5
Marcus Pontes <marcusporama@gmail.com>
2021-07-21 08:06:26 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code>src/App.js</code> 
         <a
           className="App-link"
           href="https://reactjs.org"
