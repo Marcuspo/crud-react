a4b8dad0211817b329281dbf5c813fb6e38ee135
Marcus Pontes <marcusporama@gmail.com>
2021-07-24 23:56:14 -0300

    atualização no manifest.json, cadastro.js e services.js

--- src/Components/Dashbord/Cadastro/Cadastro.js
+++ src/Components/Dashbord/Cadastro/Cadastro.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import history from "../../Routes/history"
+
+import api from "../../Services/services"
 
 import { CadastroDiv } from "./Styles"
 
 function Cliente() {
+  const [usuario, setUsuario] = useState([])
+  const [page, setPage] = useState([1])
+
+  useEffect(async () => {
+    const response = await api.get
+  }, [])
+
   function handleBackToDash() {
     history.push("/dashboard")
     window.location.reload(true)
