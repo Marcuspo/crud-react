d321705a0f3d81e64eb7be1c655bd4d4c5b7fac0
Marcus Pontes <marcusporama@gmail.com>
2021-07-24 22:33:39 -0300

    ajustes no componente cadastro, nome de styles, instalação do axios e remoção do arquivo fire.js

--- src/Components/Dashbord/Cadastro/Cadastro.js
+++ src/Components/Dashbord/Cadastro/Cadastro.js
@@ -1,5 +1,25 @@
-function Cadastro() {
-  return <div>Cadastro</div>
+import { Link } from "react-router-dom"
+import history from "../../Routes/history"
+
+import { CadastroDiv } from "./Styles"
+
+function Cliente() {
+  function handleBackToDash() {
+    history.push("/dashboard")
+    window.location.reload(true)
+  }
+
+  return (
+    <CadastroDiv>
+      <nav>
+        <h2>Cadastro</h2>
+        <Link to="/dashboard" onClick={handleBackToDash}>
+          <button>Voltar</button>
+        </Link>
+      </nav>
+      Cadastro
+    </CadastroDiv>
+  )
 }
 
-export default Cadastro
+export default Cliente
